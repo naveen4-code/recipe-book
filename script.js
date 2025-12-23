4a37480c2987f4ae8dc7d09dd39c12ac2231ac31
Naveen Raj K <knaveenraj725@gmail.com>
2025-12-23 18:08:31 +0530

    Improve UI, add recipe modal view and enhance UX

--- script.js
+++ script.js
@@ -38,13 +38,35 @@ async function loadRecipes() {
 
   snapshot.forEach(doc => {
     const r = doc.data();
+
     recipeList.innerHTML += `
       <div class="recipe-card">
         <img src="${r.imageBase64}">
         <h3>${r.name}</h3>
+        <button class="view-btn"
+          onclick="openModal(
+            '${r.name}',
+            '${r.ingredients.replace(/'/g, "\\'")}',
+            '${r.steps.replace(/'/g, "\\'")}',
+            '${r.imageBase64}'
+          )">
+          View Recipe
+        </button>
       </div>
     `;
   });
 }
 
+
 loadRecipes();
+window.openModal = function(name, ingredients, steps, image) {
+  recipeModal.style.display = "block";
+  modalTitle.innerText = name;
+  modalIngredients.innerText = ingredients;
+  modalSteps.innerText = steps;
+  modalImg.src = image;
+};
+
+window.closeModal = function() {
+  recipeModal.style.display = "none";
+};
